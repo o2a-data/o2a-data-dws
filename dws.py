4ff085dfd7b9373b339ed42132ec0f9027b65f7d
N. Anselm <norbert.anselm@awi.de>
2024-02-28 12:31:05 +0100

    + class complete, id|urn doesn't matter anymore

--- dws.py
+++ dws.py
@@ -36,14 +36,6 @@ class dws:
             return json.loads(response.content)
 
     ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
-    def test(self):
-        #        print(self.REGISTRY)
-        #        print(self.DWS)
-        print(self.teststring)
-        a = self._download(self.teststring)
-        return a
-
-    ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
     def items(self, pattern: str = None):
         """
         Loads availble sensors from the data service. The optional
@@ -130,118 +122,164 @@ class dws:
         df["datetime"] = pd.to_datetime(df["datetime"])
         return df
 
+    ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
+    def item(self, code):  # , sys=None):
+        """
+        Request and parse item properties for a given item urn as "code"
+        :param code: item unique resource number (urn) or ID
+        :param sys: switch for requesting at an alternative (under development) service ## not implemented yet
+        :return: full json
+        """
+        ##        if sys == "dev": ## later
+
+        if type(code) == str:
+            url = self.REGISTRY + "/items?where=code=LIKE=" + code
+            j = self._download(url)["records"][0]
+        elif type(code) == int:
+            url = self.REGISTRY + "/items/" + str(code)
+            j = self._download(url)
+        else:
+            raise Exception("provide item urn or item ID")
+
+        ## ITEM properties
+        url = self.REGISTRY + "/items/" + str(j["id"]) + "/properties"
+        k = self._download(url)["records"]
+        j["itemProperties"] = k
+
+        return j
+
+    ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
+    def parameters(self, code):
+        """
+        Request....
+        """
+        if type(code) == str:
+            item = self.item(code)
+            code = item["id"]
+        elif type(code) == int:
+            code = code
+        else:
+            raise Exception("provide item urn or item ID")
+
+        url = self.REGISTRY + "/items/" + str(code) + "/parameters"
+        j = self._download(url)["records"]
+        return j
+
+    ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
+    def events(self, code, geo=False):
+        """
+        Requests all events of an item, returns as dict
+        :code: registry id of item
+        :geo: true == only with valid coordinates, false == all events
+        """
+        if type(code) == str:
+            item = self.item(code)
+            code = item["id"]
+        elif type(code) == int:
+            code = code
+        else:
+            raise Exception("provide item urn or item ID")
+
+        if geo is True:
+            url = (
+                self.REGISTRY
+                + "/items/"
+                + str(code)
+                + "/events?where=latitude>=-90 and latitude<=90 and longitude>=-180 and longitude<=180"
+            )
+        else:
+            url = self.REGISTRY + "/items/" + str(code) + "/events"
+
+        j = self._download(url)["records"]
+
+        ## create lookup table
+        lut = {}
+        for i in j:
+            if isinstance(i["type"], dict):
+                lut[i["type"]["@uuid"]] = i["type"]
+
+        ## enrich event info
+        for i in range(len(j)):
+            if not isinstance(j[i]["type"], dict):
+                j[i]["type"] = lut.get(j[i]["type"])
+
+        return j
+
+    ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
+    def contacts(self, code):
+        """
+        requests contacts per item
+        :code: registry id or urn of item
+        """
+        if type(code) == str:
+            item = self.item(code)
+            code = item["id"]
+        elif type(code) == int:
+            code = code
+        else:
+            raise Exception("provide item urn or item ID")
+
+        url = self.REGISTRY + "/items/" + str(code) + "/contacts"
+
+        j = self._download(url)["records"]
+
+        contacts, roles = {}, {}
+        for i in j:
+            if isinstance(i["contact"], dict):
+                contacts[i["contact"]["@uuid"]] = i["contact"]
+            if isinstance(i["role"], dict):
+                roles[i["role"]["@uuid"]] = i["role"]
+
+        for i in range(len(j)):
+            if not isinstance(j[i]["contact"], dict):
+                j[i]["contact"] = contacts.get(j[i]["contact"])
+            if not isinstance(j[i]["role"], dict):
+                j[i]["role"] = roles.get(j[i]["role"])
+
+        return j
+
+    ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
+    def subitems(self, code):
+        """
+        retrieve subitems via base url and "*"
+        :param code: item unique resource number (urn)
+        :return: full json
+        """
+        if type(code) == str:
+            code = code
+            print(code)
+        elif type(code) == int:
+            item = self.item(code)
+            code = item["code"]
+            print(code)
+        else:
+            raise Exception("provide item urn or item ID")
+
+        url = self.REGISTRY + "/items?where=code=LIKE=" + str(code) + ":*"
+        j = self._download(url)["records"]
+        return j
+
+    ## ---------------------------  ¬!"£$%^&*()_+ --------------------------- ##
+
 
 """
-code = "vessel:polarstern:pco2_go_ps:pre_xco"
+a = dws()
 s = "2024-02-19T00:00:00"
 e = "2024-02-24T00:00:00"
 agg = "hour"
-
-# _download("https://registry.o2a-data.de/rest/v2/vocables/244")
-a = dws()
-a.test()
-a.items(code)
-a.get(items=code, begin=s, end=e)  # , aggregate = 'hour', aggregateFunctions = 'max')
+aggfun = 'min'
+code = "vessel:mya_ii:moses_moblab"
+code = 8767
+a.subitems(code)
+a.item(code)
+a.parameters(code)
+a.contacts(code)
+a.events(code)
 """
 
 import sys
 
 sys.exit()
-
-
-def item(code: str):  # , sys=None):
-    """
-    Request and parse item properties for a given item urn as "code"
-    :param code: item unique resource number (urn)
-    :param sys: switch for requesting at an alternative (under development) service ## not implemented yet
-    :return: full json
-    """
-    ##        if sys == "dev": ## later
-    url = REGISTRY + "/items?where=code=LIKE=" + code
-    j = download(url)["records"][0]
-
-    ## ITEM properties
-    url = "https://registry.o2a-data.de/rest/v2/items/" + str(j["id"]) + "/properties"
-    k = download(url)["records"]
-    j["itemProperties"] = k
-
-    return j
-
-
-def parameters(code: int):
-    """
-    Request....
-    """
-    url = REGISTRY + "/items/" + str(code) + "/parameters"
-    j = download(url)["records"]
-    return j
-
-
-def events(code: int, geo=False):
-    """
-    Requests all events of an item, returns as dict
-    :code: registry id of item
-    :geo: true == only with valid coordinates, false == all events
-    """
-    if geo is True:
-        url = (
-            REGISTRY
-            + "/items/"
-            + str(code)
-            + "/events?where=latitude>=-90 and latitude<=90 and longitude>=-180 and longitude<=180"
-        )
-    else:
-        url = REGISTRY + "/items/" + str(code) + "/events"
-
-    j = download(url)["records"]
-
-    ## create lookup table
-    lut = {}
-    for i in j:
-        if isinstance(i["type"], dict):
-            lut[i["type"]["@uuid"]] = i["type"]
-
-    ## enrich event info
-    for i in range(len(j)):
-        if not isinstance(j[i]["type"], dict):
-            j[i]["type"] = lut.get(j[i]["type"])
-
-    return j
-
-
-def contacts(code: int):
-    """
-    :code: registry id of item
-    """
-    url = REGISTRY + "/items/" + str(code) + "/contacts"
-
-    j = download(url)["records"]
-
-    contacts, roles = {}, {}
-    for i in j:
-        if isinstance(i["contact"], dict):
-            contacts[i["contact"]["@uuid"]] = i["contact"]
-        if isinstance(i["role"], dict):
-            roles[i["role"]["@uuid"]] = i["role"]
-
-    for i in range(len(j)):
-        if not isinstance(j[i]["contact"], dict):
-            j[i]["contact"] = contacts.get(j[i]["contact"])
-        if not isinstance(j[i]["role"], dict):
-            j[i]["role"] = roles.get(j[i]["role"])
-
-    return j
-
-
-def subitems(code: str):
-    """
-    retrieve subitems via base url and "*"
-    :param code: item unique resource number (urn)
-    :return: full json
-    """
-    url = REGISTRY + "/items?where=code=LIKE=" + code + ":*"
-    j = download(url)["records"]
-    return j
 
 
 # a=subitems('vessel:mya_ii')
